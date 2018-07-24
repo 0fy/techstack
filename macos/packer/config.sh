192b08c8afd96ef74dd76f20456118255456e7b7
Sven Alexander Galler <sven.galler@me.com>
2018-07-24 22:33:47 +0200

    Add minimum requirements
    to get the packer started and build a complete vagrant box file with 
    debian to use with VirtualBox we need at least a packer template, 
    preseed information for the debian installer and a bash script to add 
    needed configurations to the installation at the end.

---
+++ macos/packer/config.sh
@@ -0,0 +1,18 @@
+echo "==> Installing VBoxLinuxAdditions <=="
+mount -t iso9660 /dev/sr1 /media/cdrom
+sh /media/cdrom/VBoxLinuxAdditions.run --nox11
+umount /media/cdrom
+echo "==> VBoxLinuxAdditions installed <=="
+echo "==> Configuring ssh for vagrant <=="
+cd /home/vagrant/
+mkdir -p /home/vagrant/.ssh
+chmod 0700 /home/vagrant/.ssh
+wget --no-check-certificate https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub \
+-O /home/vagrant/.ssh/authorized_keys
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant /home/vagrant/.ssh
+echo "==> ssh for vagrant configured <=="
+apt-get -y autoremove
+apt-get -y clean
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
